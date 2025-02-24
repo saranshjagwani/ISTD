588d91b3846a18c7583f4ffa1effc5c3feb921c7
Saransh Jagwani <153913982+saranshjagwani@users.noreply.github.com>
2025-02-24 21:09:05 +0530

    Update FirebaseConfig.tsx

--- src/firebase/FirebaseConfig.tsx
+++ src/firebase/FirebaseConfig.tsx
@@ -3,16 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBvfRO53OpXpC9FjkW37o9yi_Mx1lNyq6U",
-  authDomain: "ecomove-3652c.firebaseapp.com",
-  projectId: "ecomove-3652c",
-  storageBucket: "ecomove-3652c.firebasestorage.app",
-  messagingSenderId: "728467947549",
-  appId: "1:728467947549:web:0555e1309728a1305c86f4"
-};
-
-// Initialize Firebase
+//rebase
 const app = initializeApp(firebaseConfig);
 
 const fireDB=getFirestore(app);
